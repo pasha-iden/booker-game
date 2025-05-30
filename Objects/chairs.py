0ce041ae02d0de2999f9c81a41c2bc64784e5fc8
Pavel Didenko <paveldidenko36@gmail.com>
2025-05-30 15:57:49 +0300

    first room objects placed

--- Objects/chairs.py
+++ Objects/chairs.py
@@ -19,7 +19,12 @@ class Chairs:
         self.hitbox = pygame.Rect(self.x, self.y, self.x + 48, self.y + 48)
         self.number = None
 
+
     def draw(self, scene_surface, timer):
         scene_surface.blit(self.image, (self.ix, self.iy))
         # pygame.draw.rect(scene_surface, 'Orange', (self.x, self.y, 48, 48))
         # pygame.draw.rect(scene_surface, 'Pink', (self.landing_x, self.landing_y, 48, 48))
+
+
+if __name__ == '__main__':
+    pass
